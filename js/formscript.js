0c885c357285955bb3855877cdb21bbb08414abf
paulodsp <57956688+paulodsp@users.noreply.github.com>
2025-12-12 15:24:15 +0000

    removed old dropdown entry

--- js/formscript.js
+++ js/formscript.js
@@ -1,52 +1,45 @@
-var formURL =
+  var formURL =
   "https://docs.google.com/forms/d/e/1FAIpQLScF2jcjMfcak-jVHneiFiLMDnUYS2jfTvWHXLbW42bUgPNbWA/formResponse?";
+  
 
-var sendBtn = document.getElementById("sendBtn");
-
-sendBtn.onclick = function () {
-  var formEl = document.getElementById("form");
-
-  // HTML5 + Bootstrap-style validation
-  if (!formEl.checkValidity()) {
-    formEl.classList.add("was-validated");
-    return; // do not send if invalid
-  }
-
+sendBtn.onclick = function() {
   var data = {
-    "entry.66386698": document.getElementById("66386698").value,
-    "entry.413471257": document.getElementById("413471257").value,
-    "entry.1199933740": document.getElementById("1199933740").value,
-    "entry.799268612": document.getElementById("799268612").value,
-    "entry.643330730": document.getElementById("643330730").value
+    "entry.66386698": document.getElementById("entry.66386698").value,
+    "entry.413471257": document.getElementById("entry.413471257").value,
+    "entry.1199933740": document.getElementById("entry.1199933740").value,
+    "entry.799268612": document.getElementById("entry.799268612").value,
+    "entry.643330730": document.getElementById("entry.643330730").value,
   };
-
   $("#form").hide();
   $("#loading").show();
-
   postToGoogle(formURL, data);
-
-  // Redirect to thank-you page; Google receives the POST via AJAX
   window.location.href = "https://weareenergy.coop/thankyou.html";
 };
+
 
 function postToGoogle(googleURL, data) {
   $.ajax({
     url: googleURL,
-     data,
+    data: data,
     type: "POST",
     dataType: "xml",
     statusCode: {
-      0: function () {
-        // Success (Google often gives 0 because of CORS)
+      0: function() {
+        //Success message
       },
-      200: function () {
-        // Success
+      200: function() {
+        //Success Message
       }
     },
-    error: function () {
-      // Even on error, Google usually stores the response
-      $("#form").show();
-      $("#loading").hide();
-    }
+
+    error: function(){
+      // actually throws an error, but does send properly
+       $("#form").show();
+       $("#loading").hide();
+    },
   });
 }
+
+
+
+
